a654002554bb51bd76a829d7b3f7bec6f3743e58
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-11-11 18:41:39 +0100

    remove cart item with routing

--- Algebra.WebShop/Controllers/CartController.cs
+++ Algebra.WebShop/Controllers/CartController.cs
@@ -17,7 +17,6 @@ public class CartController(ApplicationDbContext context) : Controller
     }
 
     [HttpPost]
-    [HttpPost]
     public IActionResult AddToCart(int productId)
     {
         var cart = HttpContext.Session.GetCart();
@@ -53,6 +52,20 @@ public class CartController(ApplicationDbContext context) : Controller
 
         HttpContext.Session.SetCart(cart);
 
+        return RedirectToAction(nameof(Index));
+    }
+    
+    public IActionResult RemoveFromCart(int productId)
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        if (cart.Items.Any(x => x.Product.Id == productId))
+        {
+            var item = cart.Items.Single(x => x.Product.Id.Equals(productId));
+            cart.Items.Remove(item);
+        }
+
+        HttpContext.Session.SetCart(cart);
         return RedirectToAction(nameof(Index));
     }
 }
