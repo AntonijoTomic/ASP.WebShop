08d0c5592924180e9cb1ed6198d99cce3f25d86d
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-11-15 16:07:28 +0100

    order info

---
+++ Algebra.WebShop/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Algebra.WebShop.Extensions;
+using Algebra.WebShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra.WebShop.Controllers;
+
+public class OrderController : Controller
+{
+    public IActionResult Index()
+    {
+        //var cart = HttpContext.Session.GetCart();
+        ViewData["Cart"]= HttpContext.Session.GetCart();
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CreateOrder([Bind("FirstName,LastName,PhoneNumber,CustomerEmailAdress")] Order order)
+    {
+
+        return View();
+    }
+}
--- Algebra.WebShop/Models/Order.cs
+++ Algebra.WebShop/Models/Order.cs
@@ -9,11 +9,37 @@ public class Order
     [Key]
     public int Id { get; set; }
 
-    
+    [Required]
+    public DateTime DateTimeCreated { get; set; }
+
     [Required]
     [Column(TypeName = "decimal(9, 2)")]
     public decimal Total { get; set; }
 
+    [Required(ErrorMessage ="Customer's first name is required!")]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Customer's last name is required!")]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Customer's email is required!")]
+    [StringLength(50)]
+    public string CustomerEmailAdress { get; set; }
+
+
+    [Required(ErrorMessage = "Customer's phone num is required!")]
+    public string PhoneNumber { get; set; }
+
+
+    [Required(ErrorMessage = "Customer's Adress is required!")]
+    [StringLength(50)]
+    public string CustomerAdress { get; set; }
+
     [ForeignKey("OrderId")]
-    public virtual ICollection<OrderItem> Items { get; set; }   
+    public virtual ICollection<OrderItem> Items { get; set; }
+
+
+    //public string Id { get; set; }   
 }
